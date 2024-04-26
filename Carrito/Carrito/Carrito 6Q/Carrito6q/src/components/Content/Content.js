fe91b2aaf4686d779531328a638d78360eb334d1
kingxrcia21 <168211566+kingxrcia21@users.noreply.github.com>
2024-04-26 09:52:54 -0600

    Add files via upload

---
+++ Carrito/Carrito/Carrito 6Q/Carrito6q/src/components/Content/Content.js
@@ -0,0 +1,10 @@
+import './Content.css';
+function Content(){
+    return(
+        <main>
+            <h1> Content</h1>
+            <p>Seccion de contenido de la papa</p>
+        </main>
+    );
+}
+export default Content;
